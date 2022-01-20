f028ed9d789d70e4e6b95953bc8821788d461f87
Johnny Kristensen <jkr@dis-play.dk>
2022-01-21 00:10:58 +0100

    Adds isr

---
+++ pages/[val].js
@@ -0,0 +1,35 @@
+﻿import Head from 'next/head';
+
+export default function Home({ val }) {
+  return (
+    <div>
+      <Head>
+
+      </Head>
+      
+      ISR
+    </div>
+  );
+}
+
+export const getStaticPaths = async () => ({
+  paths: [],
+  fallback: 'blocking'
+});
+
+export const getStaticProps = async () => {
+  const val = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`isr`);
+    }, 4000);
+  });
+  
+  console.log(val);
+  return {
+    props: {
+      val,
+      fallback: true
+    },
+    revalidate: 1
+  };
+};
